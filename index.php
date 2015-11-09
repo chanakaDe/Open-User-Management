bd36642adec60cffe666c5e9c178495505420fa0
chanaka_de_silva <chanaka@srilanka>
2015-11-09 10:46:54 +0530

    First commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: chanaka
+ * Date: 11/9/15
+ * Time: 10:41 AM
+ */
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>User Component</title>
+</head>
+<body>
+
+</body>
+</html>
---
+++ static/app/main.js
@@ -0,0 +1,3 @@
+/**
+ * Created by chanaka on 11/9/15.
+ */
